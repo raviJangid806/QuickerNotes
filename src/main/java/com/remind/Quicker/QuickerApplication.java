59c06a98d36a6534ef3ad5a29741141c0469d116
raviJangid806 <rj9030655@gmail.com>
2024-12-03 22:32:55 +0530

    working on structure

---
+++ src/main/java/com/remind/Quicker/QuickerApplication.java
@@ -0,0 +1,13 @@
+package com.remind.Quicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remind/Quicker/QuickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remind.Quicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
